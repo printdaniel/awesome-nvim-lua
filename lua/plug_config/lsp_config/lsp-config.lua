16ab6d582972c9443e1d3121e0efd1ee0ac08b15
Daniel Suárez <dwsuarez98@gmail.com>
2023-02-20 23:08:54 -0300

    Un nuevo comienzo

---
+++ lua/plug_config/lsp_config/lsp-config.lua
@@ -0,0 +1,26 @@
+-- Use following commadn to install language server in arch using pacman, paru(AUR) and npm
+-- sudo pacman -S pyright bash-language-server lua-language-server 
+-- ccls haskell-language-server typescript-language-server 
+-- paru -S arduino-language-server cmake-language-server-git cssmodules-language-server jdtls
+
+local nvim_lsp = require('lspconfig')
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+local servers = {'tsserver', 'hls', 'html','cssls', 'lua_ls', 'bashls', 'ccls','clangd'}
+
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    flags = {
+      debounce_text_changes = 150,
+    },
+    root_dir = function() return vim.loop.cwd() end
+  }
+end
+
+require 'lspconfig'.gopls.setup{}
+require'lspconfig'.jedi_language_server.setup{}
+require'lspconfig'.bashls.setup{}
+require'lspconfig'.tsserver.setup{}

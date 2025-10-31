de94ab479011b44678b9dab561c7658735ac8408
Daniel Suárez <dwsuarez98@gmail.com>
2025-10-31 15:00:25 -0300

    update: nvim actualizado, mazon

--- lua/usrdaniel/lazy/lsp.lua
+++ lua/usrdaniel/lazy/lsp.lua
@@ -3,82 +3,89 @@ return {
     dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/nvim-cmp",
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
         "j-hui/fidget.nvim",
     },
 
     config = function()
-        local cmp = require('cmp')
+        local cmp = require("cmp")
         local cmp_lsp = require("cmp_nvim_lsp")
         local capabilities = vim.tbl_deep_extend(
             "force",
             {},
             vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
+            cmp_lsp.default_capabilities()
+        )
 
+        -- Fidget
         require("fidget").setup({})
+
+        -- Mason
         require("mason").setup()
+
+        -- Mason-LSPConfig
         require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "rust_analyzer",
-                "gopls",
-                "pyright",
-            },
-            handlers = {
-                function(server_name) -- default handler (optional)
-
-                    require("lspconfig")[server_name].setup {
-                        capabilities = capabilities
-                    }
-                end,
-
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-				    runtime = { version = "Lua 5.1" },
-                                diagnostics = {
-                                    globals = { "vim", "it", "describe", "before_each", "after_each" },
-                                }
-                            }
-                        }
-                    }
-                end,
-            }
+            ensure_installed = { "lua_ls", "pyright", "rust_analyzer", "gopls", "clangd" },
+            automatic_enable = true,
         })
 
+        -- Configuración de servidores usando lspconfig
+        local lspconfig = require("lspconfig")
+
+        -- Lua
+        lspconfig.lua_ls.setup({
+            capabilities = capabilities,
+            settings = {
+                Lua = {
+                    runtime = { version = "Lua 5.1" },
+                    diagnostics = { globals = { "vim", "it", "describe", "before_each", "after_each" } },
+                },
+            },
+        })
+
+        -- Clangd para C/C++
+        lspconfig.clangd.setup({
+            capabilities = capabilities,
+            cmd = { "clangd", "--background-index" }, -- índice en segundo plano
+        })
+
+        -- Otros servidores
+        for _, server in ipairs({ "pyright", "rust_analyzer", "gopls" }) do
+            lspconfig[server].setup({
+                capabilities = capabilities,
+            })
+        end
+
+        -- Configuración de nvim-cmp
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
         cmp.setup({
             snippet = {
                 expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                    require("luasnip").lsp_expand(args.body)
                 end,
             },
             mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+                ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+                ["<C-y>"] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete(),
             }),
             sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
+                { name = "nvim_lsp" },
+                { name = "luasnip" },
             }, {
-                { name = 'buffer' },
-            })
+                { name = "buffer" },
+            }),
         })
 
+        -- Diagnósticos flotantes
         vim.diagnostic.config({
-            -- update_in_insert = true,
             float = {
                 focusable = false,
                 style = "minimal",
--- lua/usrdaniel/remap.lua
+++ lua/usrdaniel/remap.lua
@@ -29,5 +29,3 @@ map("n", "<S-Tab>", "<Plug>(cokeline-focus-prev)", { silent = true })
 map("n", "<Tab>", "<Plug>(cokeline-focus-next)", { silent = true })
 map("n", "<Leader>p", "<Plug>(cokeline-switch-prev)", { silent = true })
 map("n", "<Leader>n", "<Plug>(cokeline-switch-next)", { silent = true })
-
-
